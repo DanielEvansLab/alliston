738694d05c394a03c4b0612d62610243e76e7681
Dan Evans <danev1@gmail.com>
2019-07-12 10:50:16 -0700

    some edits

--- GWASfilter.sh
+++ GWASfilter.sh
@@ -2,12 +2,10 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=20G
+#SBATCH --mem-per-cpu=40G
 #SBATCH --time=10:00:00
 #SBATCH --output=GWASfilter.out
 #SBATCH --job-name="GWASfilter"
-#SBATCH --mail-user=danev1@gmail.com
-#SBATCH --mail-type=ALL
 #SBATCH -p batch
 
 cd $SLURM_SUBMIT_DIR
